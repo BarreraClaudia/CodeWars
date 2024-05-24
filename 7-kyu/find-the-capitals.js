c8348354672882972b36efab425162d122df9598
BarreraClaudia <claudiabarrera.code@gmail.com>
2024-05-24 11:37:30 -0600

    given a string, returns the indices of all capital letters

---
+++ 7-kyu/find-the-capitals.js
@@ -0,0 +1,16 @@
+// Write a function that takes a single non-empty string of only lowercase and uppercase ascii letters (word) as its argument, and returns an ordered list containing the indices of all capital (uppercase) letters in the string.
+
+// Example (Input --> Output)
+// "CodEWaRs" --> [0,3,4,6]
+
+const capitals = function (word) {
+    indices = [];
+  
+    word.split('').map((char, index) => {
+      if (char === char.toUpperCase()) {
+        indices.push(index);
+      }
+    });
+  
+    return indices;
+  };
