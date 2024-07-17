11410cc18f2b0abdfb5b77bdb925e7811bdf33b2
BarreraClaudia <claudiabarrera.code@gmail.com>
2024-07-17 14:51:44 -0600

    edited comments

--- 7-kyu/a-letters-best-friend.js
+++ 7-kyu/a-letters-best-friend.js
@@ -1,22 +1,24 @@
-// Given a string, return if a given letter always appears immediately before another given letter.
+/*
+Given a string, return if a given letter always appears immediately before another given letter.
 
-// Worked Example
-// ("he headed to the store", "h", "e") ➞ True
-// # All occurences of "h": ["he", "headed", "the"]
-// # All occurences of "h" have an "e" after it.
-// # Return True
+Worked Example
+("he headed to the store", "h", "e") ➞ True
+# All occurences of "h": ["he", "headed", "the"]
+# All occurences of "h" have an "e" after it.
+# Return True
 
-// ('abcdee', 'e', 'e') ➞ False
-// # For first "e" we can get "ee"
-// # For second "e" we cannot have "ee"
-// # Return False
+('abcdee', 'e', 'e') ➞ False
+# For first "e" we can get "ee"
+# For second "e" we cannot have "ee"
+# Return False
 
-// Examples
-// ("i found an ounce with my hound", "o", "u") ➞ True
-// ("we found your dynamite", "d", "y") ➞ False
+Examples
+("i found an ounce with my hound", "o", "u") ➞ True
+("we found your dynamite", "d", "y") ➞ False
 
-// Notes
-// All sentences will be given in lowercase.
+Notes
+All sentences will be given in lowercase.
+*/
 
 function bestFriend(txt, a, b) {
     for (let i = 0; i < txt.length; i++) {
--- 7-kyu/find-the-capitals.js
+++ 7-kyu/find-the-capitals.js
@@ -1,7 +1,10 @@
-// Write a function that takes a single non-empty string of only lowercase and uppercase ascii letters (word) as its argument, and returns an ordered list containing the indices of all capital (uppercase) letters in the string.
+/*
+Write a function that takes a single non-empty string of only lowercase and uppercase ascii letters (word) as its argument, 
+and returns an ordered list containing the indices of all capital (uppercase) letters in the string.
 
-// Example (Input --> Output)
-// "CodEWaRs" --> [0,3,4,6]
+Example (Input --> Output)
+"CodEWaRs" --> [0,3,4,6]
+*/
 
 const capitals = function (word) {
     indices = [];
