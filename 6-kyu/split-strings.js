cfed53c44d63d645846e2807a4b5a4cdf58251d7
BarreraClaudia <claudiabarrera.code@gmail.com>
2024-07-22 14:36:08 -0600

    edited comments

--- 6-kyu/split-strings.js
+++ 6-kyu/split-strings.js
@@ -1,9 +1,11 @@
-// Complete the solution so that it splits the string into pairs of two characters.If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore('_').
+/*
+Complete the solution so that it splits the string into pairs of two characters.
+If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore('_').
 
-// Examples:
-// 'abc' => ['ab', 'c_']
-// 'abcdef' => ['ab', 'cd', 'ef']
-
+Examples:
+'abc' => ['ab', 'c_']
+'abcdef' => ['ab', 'cd', 'ef']
+*/
 
 function solution(str) {
     let arr = str.split('');
