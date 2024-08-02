f8fe97dd0756a8eed68ab8b29253b70286867e49
BarreraClaudia <claudiabarrera.code@gmail.com>
2024-08-02 13:19:36 -0600

    given two strings, returns a new sorted string containing letters from given strings only once

---
+++ 7-kyu/two-to-one.js
@@ -0,0 +1,26 @@
+/*
+Take 2 strings s1 and s2 including only letters from a to z. 
+Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+Examples:
+
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+*/
+
+function longest(s1, s2) {
+    let letters = [];
+    let strings = s1 + s2;
+  
+    strings.split('').forEach(letter => {
+      if (!letters.includes(letter)) {
+        letters.push(letter);
+      }
+    });
+  
+    return letters.sort().join('');
+}
